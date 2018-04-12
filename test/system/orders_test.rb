6e9e7ea144df39bf0d2d6869fb363f14d0da09e9
james-reading <jamesreading473@gmail.com>
2018-04-12 22:58:02 +0100

    Entering additional payment details

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,9 +1,21 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit orders_url
-  #
-  #   assert_selector "h1", text: "Order"
-  # end
+  test "check routing number" do
+    visit store_index_url
+
+    first('.catalog li').click_on 'Add to Cart'
+
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'James Reading'
+    fill_in 'order_address', with: '30A Sussex Place'
+    fill_in 'order_email', with: 'james@example.com'
+
+    assert_no_selector '#order_routing_number'
+
+    select 'Check', from: 'pay_type'
+
+    assert_selector "#order_routing_number"
+  end
 end
